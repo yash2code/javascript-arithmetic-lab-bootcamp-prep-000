001b01c89ed8fd60c3e78bde2c539a5cf6993270
yash chaudhary <yash240196@gmail.com>
2017-09-16 04:41:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function add(n, m) {
 function subtract(n, m) {
   return n - m
 }
+
+function multiply(n, m) {
+  return n * m
+}
+
+function divide(n, m) {
+  return n / m
+}
+
+function inc(n) {
+  n+=1
+  return n 
+}
+
+function dec(n) {
+  n-=1
+  return n 
+}
+
+
